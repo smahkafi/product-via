e47a0f98b9fab427d652d3a5652491872d7b5993
smahkafi <sakafi.m@gmail.com>
2026-01-27 14:50:26 +0600

    Image upload UI structure added

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,6 +2,13 @@ export default function AdminPage() {
     return (
         <div>
             <h1>Admin Panel</h1>
+            
+            <input type="file" />
+
+            <p></p>
+            
+            <img width="200" />
+
         </div>
     )
 }
