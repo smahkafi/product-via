44fc533777cab727d483c8035cfb9c3fe736cb80
smahkafi <sakafi.m@gmail.com>
2026-01-27 17:58:56 +0600

    hangle image change logic

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,25 @@
+"use client"
+import { useState } from "react"
+
 export default function AdminPage() {
+    const [preview, setPreview] = useState<string | null>(null);
+    const [error, setError] = useState < string | null(null);
+
+    function handleImageChange(e: React.ChangeEvent<HTMLInputElement>){
+        const file = e.target.files?.[0];
+        if (!file) return;
+
+        if (file.size > 500 * 1024) {
+            setError("File is too large. Maximum 500KB allowed.")
+            setPreview(null);
+            return
+        } else {
+            setError(null)
+        }
+
+        const imageUrl = URL.createObjectURL(file)
+        setPreview(imageUrl)
+    }
     return (
         <div>
             <h1>Admin Panel</h1>
